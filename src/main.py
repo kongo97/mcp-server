eba1f5fbe511b7bd7d5c4aa83b68572f50fa53ca
kongo <s.costantini1997@gmail.com>
2025-04-14 18:58:53 +0200

    feat: gmail example

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Modello della richiesta
+class NumberRequest(BaseModel):
+    value: int
+
+# Modello della risposta
+class DoubleResponse(BaseModel):
+    result: int
+
+# hello world
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# gmail inbox (EXAMPLE)
+@app.get("/gmail/inbox/count")
+def double_number():
+    mail_count = 1
+    return {"inbox": mail_count}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# gmail read (EXAMPLE)
+@app.get("/gmail/read/id")
+def double_number():
+    mail_content = "Dear Client, Some text Some text Some text Some text Some text..."
+    return {"content": mail_content}
